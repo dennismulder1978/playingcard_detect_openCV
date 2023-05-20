110ec5afe158f1160112850eda253c5770e09f1c
Dennis <dnns.mldr@gmail.com>
2023-05-20 22:31:06 +0200

    ongoing initializing

--- func.py
+++ func.py
@@ -1,18 +1,32 @@
 import numpy as np
 import cv2
-from keras.models import load_model
+import imutils
 
 
 def show_cam():
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1024)
 
     while True:
-        ret, frame = cap.read()
-        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        _, bw_frame = cv2.threshold(gray_frame, 100, 255, cv2.THRESH_BINARY)
-        contours, hierarchy = cv2.findContours(bw_frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        cv2.imshow('Frame', bw_frame)
+        _, frame = cap.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frame = cv2.GaussianBlur(frame, (5, 5), 0)
+
+        threshold = cv2.threshold(frame, 20, 255, cv2.THRESH_BINARY)[1]
+
+        contours = cv2.findContours(threshold.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours = imutils.grab_contours(contours)
+
+        for contour in contours:
+            M = cv2.moments(contour)
+            if M['m00'] != 0:
+                cX = int(M['m10'] / M['m00'])
+                cY = int(M['m01'] / M['m00'])
+                shape = card_detector(contour)
+                cv2.putText(frame, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+        cv2.imshow('Frame', frame)
 
         if cv2.waitKey(1) == ord('q'):
             break
@@ -21,4 +35,14 @@ def show_cam():
     cv2.destroyAllWindows()
 
 
+def card_detector(contour):
+    shape = ""
+    contour_perimeter = 0.01 * cv2.arcLength(contour, True)
+    approx_poly_curve = cv2.approxPolyDP(contour, contour_perimeter, True)
+
+    if len(approx_poly_curve) == 4:
+        (x, y, w, h) = cv2.boundingRect(approx_poly_curve)
+        shape = 'quad'
+
+    return shape
 
