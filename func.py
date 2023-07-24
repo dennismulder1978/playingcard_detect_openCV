2ad34eb98c5fb40bffaaf65a36455b0aa14c7906
Dennis <dnns.mldr@gmail.com>
2023-07-24 21:22:46 +0200

    ongoing initializing

--- func.py
+++ func.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 # variables
 FRAME_ADJUST_BLUR_LEVEL = 3
@@ -6,7 +7,7 @@ THRESHOLD_MIN_AREA = 100
 COLOR_GREEN = (165, 255, 59)
 COLOR_PINK = (165, 59, 255)
 COLOR_WHITE = (255, 255, 255)
-LINE_WIDTH = 3
+LINE_WIDTH = 2
 WINDOW_NAME_1 = 'Original'
 WINDOW_NAME_2 = 'Adjusted'
 
@@ -29,7 +30,7 @@ def show_cam(device: int = 0, width: int = 720):
 
         # find playingcards
         end_frame = find_playingcards(adjusted_frame=adjusted_frame,
-                                      original_frame=frame,
+                                      display_frame=frame,
                                       card_value_1=0,
                                       card_value_2=150
                                       )
@@ -62,13 +63,13 @@ def show_image(image: str):
     adjusted_frame = frame_adjustment(frame=frame,
                                       gray=True,
                                       threshold=True,
-                                      value1=75,
-                                      value2=150)
+                                      value1=100,
+                                      value2=255)
 
     # find playingcards
     end_frame = find_playingcards(adjusted_frame=adjusted_frame,
-                                  original_frame=frame,
-                                  card_value_1=0,
+                                  display_frame=None,
+                                  card_value_1=75,
                                   card_value_2=150
                                   )
 
@@ -105,27 +106,111 @@ def frame_adjustment(frame,
 
 
 def find_playingcards(adjusted_frame,
-                      original_frame=None,
-                      card_value_1=400,
+                      display_frame=None,
+                      card_value_1=20,
                       card_value_2=1):
-    if original_frame is None:
-        original_frame = adjusted_frame
 
-    contours = cv2.findContours(adjusted_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    contours = contours[0] if len(contours) == 2 else contours[1]
+    contours, hierarchy = cv2.findContours(adjusted_frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contour_is_card = np.zeros(len(contours), dtype=int)
 
-    for contour in contours:
-        area = cv2.contourArea(contour)
-        if area > card_value_1:
-            try:
-                original_frame = cv2.cvtColor(original_frame, cv2.COLOR_GRAY2RGB)
-            except:
-                pass
-            cv2.drawContours(original_frame,
+    for i, contour in enumerate(contours):
+        peri = cv2.arcLength(contour, True)
+        approx = cv2.approxPolyDP(contour, 0.01 * peri, True)
+
+        if (hierarchy[0][i][3] == -1) and (len(approx) == 4):  # Find 'parent' object with 4 sides
+            if display_frame is None:  # use adjusted frame to add color contours
+                display_frame = cv2.cvtColor(adjusted_frame, cv2.COLOR_GRAY2RGB)
+            # 1. draw green contour line
+            cv2.drawContours(display_frame,
                              [contour],
                              0,
                              COLOR_GREEN,
                              LINE_WIDTH)
 
-    return original_frame
+            # 2. extract card
+            # Find width and height of card's bounding rectangle
+            x, y, w, h = cv2.boundingRect(contour)
+            card = flattener(display_frame, approx, w, h)
+            cv2.imshow('isolated card', card)
+            print(w, h)
+            # 3. show corner
+            corner = card[0:int(h/7.5), 0:int(w/8)]
+            corner_zoom = cv2.resize(corner, (0, 0), fx=4, fy=4)
+            cv2.imshow('Corner', corner_zoom)
+# https://github.com/EdjeElectronics/OpenCV-Playing-Card-Detector/blob/master/Cards.py
+    cv2.line(display_frame, (133, 94), (526, 598), COLOR_PINK, LINE_WIDTH)
+    cv2.line(display_frame, (433, 96), (134, 161), COLOR_PINK, LINE_WIDTH)
+    cv2.line(display_frame, (134, 161), (222, 596), COLOR_PINK, LINE_WIDTH)
+    cv2.line(display_frame, (222, 596), (523,530), COLOR_PINK, LINE_WIDTH)
+    cv2.line(display_frame, (523,530), (433,96), COLOR_PINK, LINE_WIDTH)
+    return display_frame
+
+
+def flattener(image, pts, w, h):
+    """Flattens an image of a card into a top-down 200x300 perspective.
+    Returns the flattened, re-sized, grayed image.
+    See www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example/"""
+    temp_rect = np.zeros((4, 2), dtype="float32")
+
+    s = np.sum(pts, axis=2)
+
+    tl = pts[np.argmin(s)]
+    br = pts[np.argmax(s)]
+
+    diff = np.diff(pts, axis=-1)
+    tr = pts[np.argmin(diff)]
+    bl = pts[np.argmax(diff)]
+
+    # Need to create an array listing points in order of
+    # [top left, top right, bottom right, bottom left]
+    # before doing the perspective transform
+
+    if w <= 0.8 * h:  # If card is vertically oriented
+        temp_rect[0] = tl
+        temp_rect[1] = tr
+        temp_rect[2] = br
+        temp_rect[3] = bl
+
+    if w >= 1.2 * h:  # If card is horizontally oriented
+        temp_rect[0] = bl
+        temp_rect[1] = tl
+        temp_rect[2] = tr
+        temp_rect[3] = br
+
+    # If the card is 'diamond' oriented, a different algorithm
+    # has to be used to identify which point is top left, top right
+    # bottom left, and bottom right.
+
+    if 0.8 * h < w < 1.2 * h:  # If card is diamond oriented
+        # If furthest left point is higher than furthest right point,
+        # card is tilted to the left.
+        if pts[1][0][1] <= pts[3][0][1]:
+            # If card is titled to the left, approxPolyDP returns points
+            # in this order: top right, top left, bottom left, bottom right
+            temp_rect[0] = pts[1][0]  # Top left
+            temp_rect[1] = pts[0][0]  # Top right
+            temp_rect[2] = pts[3][0]  # Bottom right
+            temp_rect[3] = pts[2][0]  # Bottom left
+
+        # If furthest left point is lower than furthest right point,
+        # card is tilted to the right
+        if pts[1][0][1] > pts[3][0][1]:
+            # If card is titled to the right, approxPolyDP returns points
+            # in this order: top left, bottom left, bottom right, top right
+            temp_rect[0] = pts[0][0]  # Top left
+            temp_rect[1] = pts[3][0]  # Top right
+            temp_rect[2] = pts[2][0]  # Bottom right
+            temp_rect[3] = pts[1][0]  # Bottom left
+
+    maxWidth = 200
+    maxHeight = 300
+
+    # Create destination array, calculate perspective transform matrix,
+    # and warp card image
+    dst = np.array([[0, 0], [maxWidth - 1, 0], [maxWidth - 1, maxHeight - 1], [0, maxHeight - 1]], np.float32)
+    M = cv2.getPerspectiveTransform(temp_rect, dst)
+    warp = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+    warp = cv2.cvtColor(warp, cv2.COLOR_BGR2GRAY)
+
+    return warp
 
